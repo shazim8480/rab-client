a53646d51ab1938f3bf0a14c22af8acde0877645
shazim8480 <troublmaker8480@gmail.com>
2022-03-20 17:37:32 +0600

    hero section updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,15 @@ export default function Hero() {
   return (
     <div
       id="carouselExampleCaptions"
-      class="carousel slide relative"
+      className="carousel slide relative"
       data-bs-ride="carousel"
     >
-      <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+      <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
         <button
           type="button"
           data-bs-target="#carouselExampleCaptions"
           data-bs-slide-to="0"
-          class="active"
+          className="active"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
@@ -29,64 +29,63 @@ export default function Hero() {
           aria-label="Slide 3"
         ></button>
       </div>
-      <div class="carousel-inner relative w-full overflow-hidden">
-        <div class="carousel-item active relative float-left w-full">
+      <div className="carousel-inner relative w-full overflow-hidden">
+        <div className="carousel-item active relative float-left w-full">
           <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-            class="block w-full"
+            src="https://thefinancialexpress.com.bd/uploads/1504605140.jpg"
+            className="block w-full"
             alt="..."
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
         </div>
-        <div class="carousel-item relative float-left w-full">
+        <div className="carousel-item relative float-left w-full">
           <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-            class="block w-full"
+            src="https://i.redd.it/go8033yr62t31.jpg"
+            className="block w-full"
             alt="..."
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
         </div>
-        <div class="carousel-item relative float-left w-full">
+        <div className="carousel-item relative float-left w-full">
           <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-            class="block w-full"
+            src="https://cdni0.trtworld.com/w960/h540/q75/64848_AP_18362485377045_1571577100235.jpg"
+            className="block w-full"
             alt="..."
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <h5 class="text-xl">Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
+        </div>
+        <div className="carousel-caption flex justify-between items-center absolute top-1/2 font-bold">
+          {/* caption */}
+          <div className="text-left space-y-2 lg:space-y-6 lg:text-5xl ">
+            <h4>দুর্বার</h4>
+            <h4>নির্ভীক</h4>
+            <h4>অতন্দ্র প্রহরী</h4>
           </div>
+          {/* button */}
         </div>
       </div>
+
+      {/* buttons for carousel */}
       <button
-        class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
         type="button"
         data-bs-target="#carouselExampleCaptions"
         data-bs-slide="prev"
       >
         <span
-          class="carousel-control-prev-icon inline-block bg-no-repeat"
+          className="carousel-control-prev-icon inline-block bg-no-repeat"
           aria-hidden="true"
         ></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
         type="button"
         data-bs-target="#carouselExampleCaptions"
         data-bs-slide="next"
       >
         <span
-          class="carousel-control-next-icon inline-block bg-no-repeat"
+          className="carousel-control-next-icon inline-block bg-no-repeat"
           aria-hidden="true"
         ></span>
-        <span class="visually-hidden">Next</span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
